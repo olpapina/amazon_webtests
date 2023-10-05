4f83a4deebabd54e50ec6f54b4f887838f342813
olpapina <opapina@solvd.com>
2023-10-05 15:06:22 +0300

    test support was carried out

--- cypress/support/pages/giftCardPage.ts
+++ cypress/support/pages/giftCardPage.ts
@@ -4,18 +4,19 @@ import { ResultPage } from "./resultPage";
 export class GiftCardPage {
     typeOfCard: string;
     constructor() {
-        this.typeOfCard = '[class="bxc-grid__image   bxc-grid__image--light"]';
+        this.typeOfCard = '[class="bxc-grid__image   bxc-grid__image--light bxc-grid__image--beauty bxc-grid__image--beauty"]';
     }
 
     selectTypeGiftCard(typeValue) {
         let resultPage;
+        cy.get(this.typeOfCard).eq(0).click();
         switch (typeValue) {
             case 'eGift':
-                cy.get(this.typeOfCard).eq(6).click();
+                cy.get(this.typeOfCard).eq(0).click();
                 resultPage = new ProductPage();
                 break;
             case 'Mail':
-                cy.get(this.typeOfCard).eq(7).click();
+                cy.get(this.typeOfCard).eq(1).click();
                 resultPage = new ResultPage();
                 break;
             default:
